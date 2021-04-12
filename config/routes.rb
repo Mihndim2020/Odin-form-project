7162e68aa7360ea6727f781bcbaf721e36cabb41
Mihndim <jelsieprisca2016@gmail.com>
2021-04-12 18:38:20 -0400

    Add controller, new and create methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users :only [:new, :creates]
+  get 'users/new'
+  get 'users/create'
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
