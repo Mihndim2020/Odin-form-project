8eedb129197c9f96b6a889b799255941826983da
Mihndim <jelsieprisca2016@gmail.com>
2021-04-13 05:52:57 -0400

    Add private method to require and permi user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+   # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -12,4 +12,9 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  private 
+  def user_params 
+    params.require(:user).permit(:username, :email, :password)
+  end 
 end
