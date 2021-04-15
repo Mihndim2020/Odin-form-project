b6446975b1102ec41ff18d1104235ae4e257b8f8
Mihndim <jelsieprisca2016@gmail.com>
2021-04-15 18:33:58 -0400

    Add edit and update method and routes:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  def edit 
+    @user = User.find(params[:id])
+  end 
+
+  def update 
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to edit_user_path
+
+    else 
+      render :edit
+    end 
+  end
 
   private
 
